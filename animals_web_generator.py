8879f66d678f2c8632c1507a58ab1f15df1ec15a
Red.prjects <red.me1aqw@gmail.com>
2025-04-09 12:40:37 +0200

    generate card item ffor every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -12,37 +12,34 @@ def initialize_animals_data():
     animals_data = load_data('animals_data.json')
     return animals_data
 
-def get_each_animal_info(animals_data):
+def add_animals_info_and_implement_cards_into_html(animals_data):
     """
-      Gets each animal information from json file based on name, location, diet and type, and prints it.
+      Gets each animal information from json file based on name, location, diet and type, and returns it
+      and make crds for each animal.
     """
-    output = ""
+    cards_output = ''
     for animal in animals_data:
-
-
         name = animal.get("name")
         characteristics = animal.get("characteristics", {})
         diet = characteristics.get("diet")
         location = animal.get("locations", [])[0]
         animal_type = characteristics.get("type")
-        #print(animal_type)
-        output += '<li class="cards__item">'
+        cards_output += '<li class="cards__item">'
         if "name" in animal:
-            output += f"Name: {name}<br/>\n"
+            cards_output += f"Name: {name}<br>\n"
         if "locations" in animal:
-            output += f"Location: {location}<br/>\n"
+            cards_output += f"Location: {location}<br>\n"
         if "characteristics" in animal:
             if "diet" in characteristics:
-                output += f"Diet: {diet}<br/>\n"
+                cards_output += f"Diet: {diet}<br>\n"
             if "type" in characteristics:
-                output += f"Type: {animal_type}<br/>\n"
-        ...
-        output += '</li>'
+                cards_output += f"Type: {animal_type}<br>\n"
+        cards_output += '</li>'
 
-    return output
+    return cards_output
 
 
-def implement_json_into_html_file(output_info):
+def implement_json_into_html_file(cards_output):
     """
     reads the HTML template file by using reading flies method then replaces the string with
     each animals info and creates a new HTML file with modified information.
@@ -50,17 +47,16 @@ def implement_json_into_html_file(output_info):
     with open('animals_template.html', 'r') as html_file:
         animals_template = html_file.read()
 
-
-    updated_html = animals_template.replace("__REPLACE_ANIMALS_INFO__", output_info)
+    updated_html = animals_template.replace("__REPLACE_ANIMALS_INFO__", cards_output)
     with open('animals.html', 'w') as new_html_file:
-        write_animals_template = new_html_file.write(updated_html)
+        new_html_file.write(updated_html)
 
 
 def main():
     """calls all functions inside the file and runs the generator-program"""
     animals_data = initialize_animals_data()
-    output_info = get_each_animal_info(animals_data)
-    implement_json_into_html_file(output_info)
+    cards_output = add_animals_info_and_implement_cards_into_html(animals_data)
+    implement_json_into_html_file(cards_output)
 
 
 if __name__ == "__main__":
